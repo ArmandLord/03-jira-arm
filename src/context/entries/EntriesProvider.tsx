ccbaff93b54f87aa725af60b04a932f3441e10b3
Armando Perez <armandlord98@gmail.com>
2023-04-27 12:36:20 -0600

    install uuid for entriy id

--- src/context/entries/EntriesProvider.tsx
+++ src/context/entries/EntriesProvider.tsx
@@ -2,13 +2,21 @@ import { PropsWithChildren, useReducer } from "react";
 import { EntriesContext } from "./EntriesContext";
 import { entriesReducer as reducer } from "./entriesReducer";
 import { Entry } from "@/interfaces";
+import { v4 as uuidv4 } from "uuid";
 
 export interface EntriesState {
   entries: Entry[];
 }
 
 const initialState: EntriesState = {
-  entries: [],
+  entries: [
+    {
+      _id: uuidv4(),
+      description: "Prueba y Error padrino",
+      createdAt: 1630500000000,
+      status: "pending",
+    },
+  ],
 };
 
 export const EntriesProvider = ({ children }: PropsWithChildren) => {

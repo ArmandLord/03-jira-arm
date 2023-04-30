24798ae178437235e0f862d5ff42c64ed4561db1
Armando Perez <armandlord98@gmail.com>
2023-04-30 11:14:20 -0600

    imp: add drag event

--- src/components/ui/EntriesList.tsx
+++ src/components/ui/EntriesList.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo } from "react";
+import { FC, useMemo, DragEvent } from "react";
 import { EntryCard } from "./EntryCard";
 import { EntrieStatus, Entry } from "@/interfaces";
 
@@ -11,19 +11,25 @@ export const EntriesList: FC<Props> = ({ entries, status }) => {
     () => entries.filter((entry) => entry.status === status),
     [entries]
   );
+
+  const onDropEntry = (event: DragEvent<HTMLDivElement>) => {
+    const id = event.dataTransfer.getData("text/plain");
+    console.log(id);
+  };
+
   return (
-    <div>
-      <div
-        style={{
-          height: "calc(100vh - 285px)",
-          overflow: "scroll",
-          padding: "5px 0",
-        }}
-      >
-        {filteredEntries.map((entry) => (
-          <EntryCard key={entry._id} entry={entry} />
-        ))}
-      </div>
+    <div
+      onDrop={onDropEntry}
+      onDragOver={(event) => event.preventDefault()}
+      style={{
+        height: "calc(100vh - 285px)",
+        overflow: "scroll",
+        padding: "5px 0",
+      }}
+    >
+      {filteredEntries.map((entry) => (
+        <EntryCard key={entry._id} entry={entry} />
+      ))}
     </div>
   );
 };
--- src/components/ui/EntryCard.tsx
+++ src/components/ui/EntryCard.tsx
@@ -1,5 +1,5 @@
+import { FC, DragEvent } from "react";
 import { Entry } from "@/interfaces";
-import React, { FC } from "react";
 
 interface Props {
   entry: Entry;
@@ -7,8 +7,24 @@ interface Props {
 
 export const EntryCard: FC<Props> = ({ entry }) => {
   const { description, createdAt, status } = entry;
+
+  const onDragStart = (event: DragEvent<HTMLDivElement>) => {
+    // hacer efectos
+    event.dataTransfer.setData("text/plain", entry._id);
+  };
+
+  const onDragEnd = () => {
+    console.log("drag end");
+  };
+
   return (
-    <div className="bg-background-three p-4 mb-1 shadow rounded">
+    <div
+      draggable
+      onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
+      id={entry._id}
+      className="bg-background-three p-4 mb-1 shadow rounded"
+    >
       <div className="flex justify-between">
         <h3 className="text-xl font-bold mb-2">{description}</h3>
         <p className="text-gray-600">{status}</p>

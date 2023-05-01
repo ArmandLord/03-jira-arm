4bc2aaa498900a36e64d50d12ae881ff86a1bbe3
Armando Perez <armandlord98@gmail.com>
2023-05-01 10:22:55 -0600

    create image docker

---
+++ database/db.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const mongoConection = {
+    isConnected: 0,
+}
+
+export const connectDB = async () => {
+
+    if (mongoConection.isConnected) {
+        console.log('ya esta conectado');
+        return;
+    }
+
+
+    if (mongoose.connections.length > 0) {
+        mongoConection.isConnected = mongoose.connections[0].readyState;
+        if (mongoConection.isConnected === 1) {
+            console.log('usando conexion anterior');
+            return;
+        }
+    }
+
+    await mongoose.connect('')
+    mongoConection.isConnected = 1;
+    console.log('conectado a MongoDB:');
+};
+
+
+export const disconnectDB = async () => {
+    if(mongoConection.isConnected !== 0) return;
+
+    await mongoose.disconnect();
+    console.log('Desconectado de MongoDB');
+}
